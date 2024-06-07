e6c408d5e0de85f8102ffaaf07ad5368bd459a77
darkmat13r <darkmat13r@gmail.com>
2024-06-07 20:39:49 +0530

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.drawgestures.learnopengl"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2"
         ndk {
             abiFilters.add("arm64-v8a")
         }
